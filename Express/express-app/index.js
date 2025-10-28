0488d60938921e3e37a543545ffb6b81352a70e7
WIN11 <adnan.codesymphony@gmail.com>
2025-10-28 12:12:12 +0530

    pagination

--- Express/express-app/index.js
+++ Express/express-app/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const data = require('./data.json');
+const user = require('./user.json');
+
+app.get('/',(req,res)=>{
+    res.send("Welcome to Express");
+});
 
 app.get('/find',(req,res)=>{
     res.send(data);
@@ -10,7 +15,24 @@ app.get('/find/:id',(req,res)=>{
     res.send(data[(req.params.id)-1]);
 });
 
-console.log(data);
+
+app.get('/user/:userId',(req,res)=>{
+    let uId = req.params.userId;
+    let txt = "";
+
+    user.map((user,index)=>{
+    
+        if(user.userId == uId)
+        {
+             txt += `<br>${user.id}=========${user.title}========${user.body}`;    
+        }
+         
+    });
+
+    res.send(txt);
+
+});
+
 
 
 app.listen(port, ()=>{
