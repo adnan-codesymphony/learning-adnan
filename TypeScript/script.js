69508a27c534048590f6a4ab6f03215cec938cbb
WIN11 <adnan.codesymphony@gmail.com>
2025-10-31 17:25:58 +0530

    TypeScript Practice

---
+++ TypeScript/script.js
@@ -0,0 +1,9 @@
+// Number, String , Boolean
+var a = 10;
+var str = "Amit";
+var flag = false;
+console.log(a, str, flag);
+// Array , Tuple
+var arr = [1, 2, 3, 4];
+var tuple = [1, "Amit", false];
+console.log("".concat(arr, "\n").concat(tuple));
---
+++ TypeScript/script.ts
@@ -0,0 +1,23 @@
+// Number, String , Boolean
+
+var a: number = 10;
+var str: string = "Amit";
+var flag: boolean = false;
+
+console.log(a, str, flag)
+ 
+// Array , Tuple
+var arr = [1, 2, 3, 4] 
+var tuple : [number, string, boolean] = [1, "Amit", false]
+
+console.log(`${arr}\n${tuple}`)
+
+// enum
+/* enum Direction{
+
+    top = "TOP",
+    left = "LEFT",
+    right = "RIGHT",
+    bottom = "BOTTOM"
+}
+ */
